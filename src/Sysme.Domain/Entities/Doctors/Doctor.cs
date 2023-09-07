968b23c22096f8c225028eef76b4a9a8dee1f31d
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2023-09-07 11:50:13 +0500

    Schedule and Appointment added

--- src/Sysme.Domain/Entities/Doctors/Doctor.cs
+++ src/Sysme.Domain/Entities/Doctors/Doctor.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sysme.Domain.Commons;
+using Sysme.Domain.Enums;
 
-namespace Sysme.Domain.Entities.Doctors
+namespace Sysme.Domain.Entities.Doctors;
+
+public class Doctor : AudiTable
 {
-    internal class Doctor
-    {
-    }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Specialty { get; set; }
+    public Gender Gender { get; set; }
 }
