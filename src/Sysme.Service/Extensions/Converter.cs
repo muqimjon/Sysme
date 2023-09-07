c1749b41cdbfb551d715a15fd57f7928cbdb4ad6
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2023-09-07 18:02:45 +0500

    Add DoctorController

--- src/Sysme.Service/Extensions/Converter.cs
+++ src/Sysme.Service/Extensions/Converter.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sysme.Service.Extensions
+namespace Sysme.Service.Extensions;
+
+public static class Converter
 {
-    internal class Converter
+    public static byte[] ToByte(this IFormFile formFile)
     {
+        using var memoryStream = new MemoryStream();
+        formFile.CopyTo(memoryStream);
+        return memoryStream.ToArray();
     }
 }
