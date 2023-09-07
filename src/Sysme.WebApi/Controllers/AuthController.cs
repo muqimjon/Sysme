49f0feed5f12840ed49f1bb6eb44acde4a6902a3
jakhangir-esanov <alibaba.esanov@gmail.com>
2023-09-07 15:40:29 +0300

    Added Auth controller

---
+++ src/Sysme.WebApi/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sysme.Service.Interfaces;
+using Sysme.WebApi.Controllers.Commons;
+using Sysme.WebApi.Models;
+
+namespace Sysme.WebApi.Controllers;
+
+public class AuthController : BaseController
+{
+    private readonly IAuthService authService;
+    public AuthController(IAuthService authService)
+    {
+        this.authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> GenerateTokenAsync(string email, string password)
+       => Ok(new Response { Data = await authService.GenerateTokenAsync(email, password) });
+}
