d8568d7706a48c0d7430d6d59ddffd37321d7c4a
abdurrohmanq <qodirovabdurahmon46@gmail.com>
2023-09-07 10:42:42 +0500

    Patient,Doctor,Hospital models wrote

---
+++ src/Sysme.Domain/Entities/Patients/Patient.cs
@@ -0,0 +1,13 @@
+﻿using Sysme.Domain.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sysme.Domain.Entities.Patients;
+
+public class Patient : AudiTable
+{
+    public string FirstName { get; set; }
+}
