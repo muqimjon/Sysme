0d617cd33d783edc3a3563c104a25f313cb4e24e
jakhangir-esanov <alibaba.esanov@gmail.com>
2023-09-07 12:51:53 +0300

    "Added AutoMapper"

---
+++ src/Sysme.Service/Mappers/MappingProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Sysme.Domain.Entities.Appointments;
+using Sysme.Domain.Entities.Doctors;
+using Sysme.Domain.Entities.Hospitals;
+using Sysme.Domain.Entities.Patients;
+using Sysme.Domain.Entities.Schedules;
+
+namespace Sysme.Service.Mappers;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        //Appointment
+        CreateMap<Appointment, AppointmentCreationDto>().ReverseMap();
+        CreateMap<AppointmentUpdateDto, Appointment>().ReverseMap();
+        CreateMap<AppointmentResultDto, Appointment>().ReverseMap();
+
+        //Doctor
+        CreateMap<Doctor, DoctorCreationDto>().ReverseMap();
+        CreateMap<DoctorUpdateDto, Doctor>().ReverseMap();
+        CreateMap<DoctorResultDto, Doctor>().ReverseMap();
+
+        //Hospital
+        CreateMap<Hospital, HospitalCreationDto>.ReverseMap();
+        CreateMap<HospitalUpdateDto, Hospital>.ReverseMap();
+        CreateMap<HospitalResultDto, Hospital>.ReverseMap();
+
+        //Patient
+        CreateMap<Patient, PatientCreationDto>.ReverseMap();
+        CreateMap<PatientUpdateDto, Patient>.ReverseMap();
+        CreateMap<PatientResultDto, Patient>.ReverseMap();
+
+        //Schedule
+        CreateMap<Schedule, ScheduleCreationDto>.ReverseMap();
+        CreateMap<ScheduleUpdateDto, Schedule>.ReverseMap();
+        CreateMap<ScheduleResultDto, Schedule>.ReverseMap();
+    }
+}
