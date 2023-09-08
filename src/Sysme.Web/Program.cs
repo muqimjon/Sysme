5b6687e1ede63210282a6f5ad82fda0a3f0a6fc1
Muqimjon <muqimjon.mr@gmail.com>
2023-09-08 15:38:47 +0500

    Changed design

--- src/Sysme.Web/Program.cs
+++ src/Sysme.Web/Program.cs
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=doctors}/{action=Index}/{id?}");
 
 app.Run();
